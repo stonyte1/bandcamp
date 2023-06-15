9f525940a5a8572a4b1a9b7f4c824141a231c309
Evelina Stonyte <stonyteevelina@gmail.com>
2023-06-15 14:14:11 +0300

    home page and template made

--- bandcamp/music/views.py
+++ bandcamp/music/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 def home(request):
-    return render(request, 'base.html', {"posts":"sveiki"})
+    return render(request, 'music/home.html', {'music': 'song'})
