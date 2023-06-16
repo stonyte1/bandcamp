cf70d40a9ff93f877f9eff7b444611ad7d968b20
Evelina Stonyte <stonyteevelina@gmail.com>
2023-06-16 08:41:57 +0300

    updated models and created album detail

--- bandcamp/music/models.py
+++ bandcamp/music/models.py
@@ -2,29 +2,6 @@ from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
-
-class Song(models.Model):
-    title = models.CharField(_("title"), max_length=250)
-    audio = models.FileField(_("audio"), upload_to='music/', blank=True, null=True)
-    duration = models.CharField(_("duration"), max_length=5)
-    album = models.ForeignKey(
-        'Album',
-        verbose_name=_("album"),
-        on_delete=models.CASCADE,
-        related_name='songs'
-    )
-    
-    class Meta:
-        verbose_name = _("song")
-        verbose_name_plural = _("songs")
-
-    def __str__(self):
-        return f'{self.title}'
-
-    def get_absolute_url(self):
-        return reverse("song_detail", kwargs={"pk": self.pk})
-
-
 class Album(models.Model):
     title = models.CharField(_("title"), max_length=250)
     cover = models.ImageField(_("cover"), upload_to='music/')
@@ -51,3 +28,27 @@ class Album(models.Model):
 
     def get_absolute_url(self):
         return reverse("album_detail", kwargs={"pk": self.pk})
+
+class Song(models.Model):
+    title = models.CharField(_("title"), max_length=250)
+    audio = models.FileField(_("audio"), upload_to='music/', blank=True, null=True)
+    duration = models.CharField(_("duration"), max_length=5)
+    album = models.ForeignKey(
+        Album,
+        verbose_name=_("album"),
+        on_delete=models.CASCADE,
+        related_name='songs'
+    )
+    
+    class Meta:
+        verbose_name = _("song")
+        verbose_name_plural = _("songs")
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def get_absolute_url(self):
+        return reverse("song_detail", kwargs={"pk": self.pk})
+
+
+
