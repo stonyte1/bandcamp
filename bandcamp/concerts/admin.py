9406fbf7042bea420b5b178b2dda3924cdfe467b
Honichiwa <tadas.zupka@gmail.com>
2023-06-19 15:53:30 +0300

    concert admin views, full logic for ticket_status

--- bandcamp/concerts/admin.py
+++ bandcamp/concerts/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from . import models
 
+class ConcertsAdmin(admin.ModelAdmin):
+    list_display = ('date', 'city', 'venue', 'ticket_status', 'link', 'is_visible')
+    list_editable = ('ticket_status',) 
+
+    def is_visible(self, obj):
+        return obj.visible
+    
+    is_visible.boolean = True
 
 admin.site.register(models.ConcertCity)
 admin.site.register(models.ConcertVenue)
-admin.site.register(models.Concert)
+admin.site.register(models.Concert, ConcertsAdmin)
