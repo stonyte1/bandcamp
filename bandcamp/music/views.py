b336f7231d793f099911e702615cc48e07c874b7
Evelina Stonyte <stonyteevelina@gmail.com>
2023-06-20 10:45:18 +0300

    Album creation form and page created

--- bandcamp/music/views.py
+++ bandcamp/music/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
-from .models import Album, Song
+from django.forms.models import BaseModelForm
+from .models import Album
+from .forms import AlbumForm
+from django.contrib import messages
+from django.core.files import File
+
+
 
 
 def home(request):
     return render(request, 'music/home.html')
+
+def album(request):
+    if request.method == 'POST':
+        form = AlbumForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            title = form.cleaned_data.get('title')
+            messages.success(request, f'Album {title} added')
+            return redirect('music')
+        else:
+            # Print form errors to the console
+            print(form.errors)
+    else:
+        form = AlbumForm
+
+    return render(request, 'music/manage_album.html', {'form': form})
 
 class AlbumList(generic.ListView):
     model = Album
@@ -14,4 +36,6 @@ class AlbumList(generic.ListView):
 class AlbumDetailView(generic.DetailView):
     model = Album
     template_name = 'music/album_detail.html'
+
+
 
